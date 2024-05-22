bfa2e0c741b56245b9de3f6677f271622583dae4
harsh <hk.khare5@gmail.com>
2024-05-22 16:56:01 +0530

    added school list om the basis of user level

---
+++ src/services/nonacademics/school.service.js
@@ -0,0 +1,30 @@
+const { Location } = require('../../models')
+
+
+const getSchoolsList = async (cluster, level) => {
+    try {
+        let schoolsArray = [];
+        if(level === "Cluster") {
+            const locations = await Location.find({ cluster: cluster });
+            schoolsArray = locations.map((location) => ({ 
+                schoolName: location.schoolName,
+                displayName: location.displayName,
+                centreCode: location.centreCode
+            }))
+        } else if(level === "Vertex") {
+            const locations = await Location.find({ });
+            schoolsArray = locations.map((location) => ({ 
+                schoolName: location.schoolName,
+                displayName: location.displayName,
+                centreCode: location.centreCode
+            }))
+        }
+        return schoolsArray;
+    } catch (error) {
+        throw new Error(`Failed: ${error.message}`);
+    }
+}
+
+module.exports = {
+    getSchoolsList
+}
